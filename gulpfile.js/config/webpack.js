109a93744e7248f316537202ef4a12a979227cb9
Andrew <andrew@huth.me>
2015-08-09 11:33:48 -0700

    Remove example Gulp pages, scripts, and icons

--- gulpfile.js/config/webpack.js
+++ gulpfile.js/config/webpack.js
@@ -31,8 +31,6 @@ module.exports = function(env) {
   if(env !== 'test') {
     // Karma doesn't need entry points or output settings
     webpackConfig.entry= {
-      page1: [ './page1.js' ],
-      page2: [ './page2.js' ]
     }
 
     webpackConfig.output= {
