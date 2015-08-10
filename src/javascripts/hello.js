ea29009588d6242d67ae25f4ef78f9637eba3d05
Andrew <andrew@huth.me>
2015-08-09 20:21:25 -0700

    Add basic React component that says "Hello World!"
    Making sure we have React setup correctly.

---
+++ src/javascripts/hello.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var React = require("react");
+
+var Hello = React.createClass({
+  render: function () {
+    return <h1>Hello World!</h1>;
+  }
+});
+
+module.exports = Hello;
--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -1,1 +1,12 @@
-console.log("hello world")
+"use strict";
+
+var React = require("react");
+var Hello = require("./hello");
+
+var Index = React.createClass({
+  render: function () {
+    return <Hello />;
+  }
+});
+
+React.render(<Index />, document.getElementById("content"));
