5c7d7e4c0e8dec61f812a23357fcc3e6621984d4
Andrew <andrew@huth.me>
2015-08-16 21:39:36 -0700

    Tighten up variable name in <Card>
    Both objects that were changed contain mappings (plural), so we’ve
    changed the variable names to reflect that.

--- src/javascripts/components/card.js
+++ src/javascripts/components/card.js
@@ -2,13 +2,13 @@
 
 var React = require("react");
 
-var faceMapping = {four: 4, six: 6, nine: 9, ten: 10, jack: "J", queen: "Q", king: "K", ace: "A"};
-var suitMapping = {clubs: "C", diamonds: "D", hearts: "H", spades: "S"};
+var faceMappings = {four: 4, six: 6, nine: 9, ten: 10, jack: "J", queen: "Q", king: "K", ace: "A"};
+var suitMappings = {clubs: "C", diamonds: "D", hearts: "H", spades: "S"};
 
 var Card = React.createClass({
   getCardId: function () {
-    const face = faceMapping[this.props.face];
-    const suit = suitMapping[this.props.suit];
+    const face = faceMappings[this.props.face];
+    const suit = suitMappings[this.props.suit];
     return `${face}${suit}`;
   },
 
