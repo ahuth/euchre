f9fed08689c48bd5bf2f4e172fe845329c3d5d7c
Andrew <andrew@huth.me>
2015-10-29 22:16:50 -0700

    Remove source maps
    I haven’t found (yet) source maps to be that useful. Also, we were only
    using them for SASS, and we have so little styling that it’s not worth
    it.

--- gulpfile.js/tasks/sass.js
+++ gulpfile.js/tasks/sass.js
@@ -1,16 +1,13 @@
 var gulp         = require('gulp');
 var browserSync  = require('browser-sync');
 var sass         = require('gulp-sass');
-var sourcemaps   = require('gulp-sourcemaps');
 var handleErrors = require('../lib/handleErrors');
 var config       = require('../config/sass');
 
 gulp.task('sass', function () {
   return gulp.src(config.src)
-    .pipe(sourcemaps.init())
     .pipe(sass(config.settings))
     .on('error', handleErrors)
-    .pipe(sourcemaps.write())
     .pipe(gulp.dest(config.dest))
     .pipe(browserSync.reload({stream:true}));
 });
