7a374691bf12a81355d370c5f2d6c9f5a42122cc
Andrew <andrew@huth.me>
2015-10-31 20:05:52 -0700

    Remove card from hand after clicking it

--- spec/reducers/hands-spec.js
+++ spec/reducers/hands-spec.js
@@ -13,6 +13,22 @@ describe("hands", function () {
     expect(output.west.length).toBe(5);
   });
 
+  it("responds to SELECT_CARD by removing that card from itself", function () {
+    var currentState = {
+      north: [{ face: "ace", suit: "clubs" }, { face: "king", suit: "diamonds" }],
+      south: [{ face: "ten", suit: "spades" }],
+      east: [{ face: "queen", suit: "diamonds" }],
+      west: [{ face: "jack", suit: "hearts" }, { face: "jack", suit: "diamonds" }]
+    };
+    var action = actions.selectCard({face: "ace", suit: "clubs"});
+    expect(hands(currentState, action)).toEqual({
+      north: [{ face: "king", suit: "diamonds" }],
+      south: [{ face: "ten", suit: "spades" }],
+      east: [{ face: "queen", suit: "diamonds" }],
+      west: [{ face: "jack", suit: "hearts" }, { face: "jack", suit: "diamonds" }]
+    })
+  });
+
   it("responds to any other action by returning the current state", function () {
     var currentState = {
       north: [{ card: "a" }],
