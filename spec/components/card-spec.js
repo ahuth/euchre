e39cf7a96fdbb3f71fbd2eafe19e8bd1ada0339a
Andrew <andrew@huth.me>
2015-12-04 19:11:56 -0800

    Remove unnecessary ReactDom require in the Card tests
    No longer needed since we’re doing shallow rendering, now.

--- spec/components/card-spec.js
+++ spec/components/card-spec.js
@@ -1,7 +1,6 @@
 "use strict";
 
 var React = require("react");
-var ReactDOM = require("react-dom");
 var ReactTestUtils = require('react-addons-test-utils');
 var Card = require("../../src/javascripts/components/card");
 
