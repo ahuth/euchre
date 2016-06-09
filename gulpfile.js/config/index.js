116f5c57727585438c3981cfa5a26c31de99d4a2
Andrew <andrew@huth.me>
2016-06-08 19:39:30 -0700

    Remove gulp dependencies and stuff

--- gulpfile.js/config/index.js
+++
@@ -1,9 +0,0 @@
-var config = {}
-
-config.publicDirectory = "./public"
-config.sourceDirectory = "./src"
-config.testDirectory = "./spec"
-config.publicAssets    = config.publicDirectory
-config.sourceAssets    = config.sourceDirectory
-
-module.exports = config
