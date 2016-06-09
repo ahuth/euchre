ce16daa3f01b6a26e3b76abe3bf0a6fbdd80cef4
Andrew <andrew@huth.me>
2016-06-08 23:28:55 -0700

    Update Webpack config to be basic one

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,23 @@
-module.exports = require('./gulpfile.js/config/webpack')('development')
+module.exports = {
+  entry: "./src/javascripts/index.js",
+
+  output: {
+    path: "./public/js",
+    filename: "app.js"
+  },
+
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: "babel",
+      query: {
+        presets: ["es2015", "react"]
+      }
+    }]
+  },
+
+  resolve: {},
+
+  plugins: []
+};
