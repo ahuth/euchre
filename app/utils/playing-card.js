2f3b5fee9df0eea169349575e20db41c8d63085f
Andrew <andrew@huth.me>
2016-06-12 16:03:13 -0700

    Add PlayingCard object

---
+++ app/utils/playing-card.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const faceMapping = {nine: "9", ten: "10", jack: "J", queen: "Q", king: "K", ace: "A"};
+const suitMapping = {spades: "♠", clubs: "♣", hearts: "♥", diamonds: "♦"};
+
+function PlayingCard({face, suit}) {
+  this.face = face;
+  this.suit = suit;
+}
+
+PlayingCard.prototype.toString = function () {
+  const faceCharacter = faceMapping[this.face];
+  const suitCharacter = suitMapping[this.suit];
+  return `${faceCharacter}${suitCharacter}`;
+};
+
+PlayingCard.prototype.color = function () {
+  const suitCharacter = suitMapping[this.suit];
+  return isRed(suitCharacter) ? "red" : "black";
+};
+
+function isRed(suitCharacter) {
+  return [suitMapping.hearts, suitMapping.diamonds].indexOf(suitCharacter) >= 0;
+}
+
+export default PlayingCard;
