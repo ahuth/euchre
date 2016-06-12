4639926e72efa7f058e72f32660c0ec5404057a9
Andrew <andrew@huth.me>
2016-06-12 15:24:23 -0700

    Refactor hands reducer tests
    Split up our dealHands tests so that each one has only one reason to
    fail.

--- spec/reducers/hands-spec.js
+++ spec/reducers/hands-spec.js
@@ -4,18 +4,29 @@ import hands from "reducers/hands";
 import {dealHands} from "../../app/actions";
 
 describe("hands", function () {
-  it("deals 5 random cards for dealHands", function () {
-    var action = dealHands();
-    var output = hands(null, action);
-    expect(output.south.length).toBe(5);
-    expect(output.south[0].face).toBeTruthy();
-    expect(output.south[0].suit).toBeTruthy();
-  });
-
-  it("returns the current state for any unknown action", function () {
+  it("returns the current state for unknown actions", function () {
     var currentState = {
       south: [{face: "queen", suit: "hearts"}]
     }
     expect(hands(currentState, {type: "WAT"})).toEqual(currentState);
+  });
+
+  describe("for dealHands", function () {
+    var action;
+
+    beforeEach(function () {
+      action = dealHands();
+    });
+
+    it("deals 5 objects", function () {
+      var output = hands(null, action);
+      expect(output.south.length).toBe(5);
+    });
+
+    it("deals objects that represents cards", function () {
+      var output = hands(null, action);
+      expect(output.south[0].face).toBeTruthy();
+      expect(output.south[0].suit).toBeTruthy();
+    });
   });
 });
