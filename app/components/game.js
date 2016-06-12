e40a60a897cd628edeaad3b8d6dd1da03532691c
Andrew <andrew@huth.me>
2016-06-12 05:18:55 -0700

    Convert <Game> into a full class and link up the store

--- app/components/game.js
+++ app/components/game.js
@@ -2,9 +2,28 @@
 
 import Card from "./card";
 import React from "react";
+import store from "../store";
 
-function Game() {
-  return <Card face="ten" suit="spades" />;
-}
+var Game = React.createClass({
+  getInitialState: function () {
+    return store.getState();
+  },
+
+  componentWillMount: function () {
+    store.unsubscribe = store.subscribe(this.onChange);
+  },
+
+  componentWillUnmount: function () {
+    store.unsubscribe();
+  },
+
+  onChange: function () {
+    this.setState(store.getState());
+  },
+
+  render: function () {
+    return <Card face="ten" suit="spades" />;
+  }
+});
 
 export default Game;
