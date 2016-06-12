369073c7f376d2d4f0846086797fbb42fc602515
Andrew <andrew@huth.me>
2016-06-12 01:44:55 -0700

    Add the karma mocha reporter

--- karma.conf.js
+++ karma.conf.js
@@ -50,7 +50,11 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['mocha'],
+
+    mochaReporter: {
+      output: 'autowatch'
+    },
 
 
     // web server port
