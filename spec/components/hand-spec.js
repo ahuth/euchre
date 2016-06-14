5897dc66cfcee7d0217f840a80226923481b6c6c
Andrew <andrew@huth.me>
2016-06-13 22:01:57 -0700

    Remove unnecessary temp variables from tests

--- spec/components/hand-spec.js
+++ spec/components/hand-spec.js
@@ -7,18 +7,16 @@ import {jack} from "utils/playing-cards";
 import {shallow} from "enzyme";
 
 describe("<Hand />", function () {
-  var component, right, left;
+  var component;
 
   beforeEach(function () {
-    right = jack.clubs;
-    left = jack.spades;
-    component = shallow(<Hand cards={[right, left]} />);
+    component = shallow(<Hand cards={[jack.clubs, jack.spades]} />);
   });
 
   it("renders a <Card> for each card", function () {
     var cards = component.find("Card")
     expect(cards.length).toBe(2);
-    expect(cards.first().equals(<Card card={right} />)).toBe(true);
-    expect(cards.last().equals(<Card card={left} />)).toBe(true);
+    expect(cards.first().equals(<Card card={jack.clubs} />)).toBe(true);
+    expect(cards.last().equals(<Card card={jack.spades} />)).toBe(true);
   });
 });
