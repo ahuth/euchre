ed56fd698a7f1bcd2b3affa1468de39e5f1bbe1b
Andrew <andrew@huth.me>
2016-06-18 21:07:38 -0700

    Add top level describe for utils

--- spec/utils/deck-spec.js
+++ spec/utils/deck-spec.js
@@ -3,36 +3,38 @@
 import Deck from "utils/deck";
 import PlayingCard from "utils/playing-card";
 
-describe("Deck", function () {
-  var deck;
+describe("utils", function () {
+  describe("Deck", function () {
+    var deck;
 
-  beforeEach(function () {
-    deck = new Deck();
-  });
-
-  describe("#draw", function () {
-    it("returns an array of playing cards", function () {
-      var cards = deck.draw(4);
-      expect(cards.length).toBe(4);
-      expect(cards[0] instanceof PlayingCard).toBe(true);
+    beforeEach(function () {
+      deck = new Deck();
     });
 
-    it("returns any number of cards", function () {
-      var cards = deck.draw(7);
-      expect(cards.length).toBe(7);
-    });
+    describe("#draw", function () {
+      it("returns an array of playing cards", function () {
+        var cards = deck.draw(4);
+        expect(cards.length).toBe(4);
+        expect(cards[0] instanceof PlayingCard).toBe(true);
+      });
 
-    it("returns one card by default", function () {
-      var cards = deck.draw();
-      expect(cards.length).toBe(1);
-    });
+      it("returns any number of cards", function () {
+        var cards = deck.draw(7);
+        expect(cards.length).toBe(7);
+      });
 
-    it("stops returning cards once they're gone", function () {
-      var numCards = deck.cards.length
-      expect(numCards).toBeGreaterThan(0);
-      deck.draw(numCards - 1);
-      var cards = deck.draw(3);
-      expect(cards.length).toBe(1);
+      it("returns one card by default", function () {
+        var cards = deck.draw();
+        expect(cards.length).toBe(1);
+      });
+
+      it("stops returning cards once they're gone", function () {
+        var numCards = deck.cards.length
+        expect(numCards).toBeGreaterThan(0);
+        deck.draw(numCards - 1);
+        var cards = deck.draw(3);
+        expect(cards.length).toBe(1);
+      });
     });
   });
 });
--- spec/utils/playing-card-spec.js
+++ spec/utils/playing-card-spec.js
@@ -2,90 +2,92 @@
 
 import PlayingCard from "utils/playing-card";
 
-describe("PlayingCard", function () {
-  var playingCard;
+describe("utils", function () {
+  describe("PlayingCard", function () {
+    var playingCard;
 
-  describe("the nine of spades", function () {
-    beforeEach(function () {
-      playingCard = new PlayingCard({face: "nine", suit: "spades"});
+    describe("the nine of spades", function () {
+      beforeEach(function () {
+        playingCard = new PlayingCard({face: "nine", suit: "spades"});
+      });
+
+      it("can be converted into a string", function () {
+        expect(playingCard.toString()).toBe("9♠")
+      });
+
+      it("indicates its color", function () {
+        expect(playingCard.color()).toBe("black");
+      });
     });
 
-    it("can be converted into a string", function () {
-      expect(playingCard.toString()).toBe("9♠")
+    describe("the 10 of clubs", function () {
+      beforeEach(function () {
+        playingCard = new PlayingCard({face: "ten", suit: "clubs"});
+      });
+
+      it("can be converted into a string", function () {
+        expect(playingCard.toString()).toBe("10♣")
+      });
+
+      it("indicates its color", function () {
+        expect(playingCard.color()).toBe("black");
+      });
     });
 
-    it("indicates its color", function () {
-      expect(playingCard.color()).toBe("black");
-    });
-  });
+    describe("the jack of hearts", function () {
+      beforeEach(function () {
+        playingCard = new PlayingCard({face: "jack", suit: "hearts"});
+      });
 
-  describe("the 10 of clubs", function () {
-    beforeEach(function () {
-      playingCard = new PlayingCard({face: "ten", suit: "clubs"});
-    });
+      it("can be converted into a string", function () {
+        expect(playingCard.toString()).toBe("J♥")
+      });
 
-    it("can be converted into a string", function () {
-      expect(playingCard.toString()).toBe("10♣")
-    });
-
-    it("indicates its color", function () {
-      expect(playingCard.color()).toBe("black");
-    });
-  });
-
-  describe("the jack of hearts", function () {
-    beforeEach(function () {
-      playingCard = new PlayingCard({face: "jack", suit: "hearts"});
+      it("indicates its color", function () {
+        expect(playingCard.color()).toBe("red");
+      });
     });
 
-    it("can be converted into a string", function () {
-      expect(playingCard.toString()).toBe("J♥")
+    describe("the queen of diamonds", function () {
+      beforeEach(function () {
+        playingCard = new PlayingCard({face: "queen", suit: "diamonds"});
+      });
+
+      it("can be converted into a string", function () {
+        expect(playingCard.toString()).toBe("Q♦")
+      });
+
+      it("indicates its color", function () {
+        expect(playingCard.color()).toBe("red");
+      });
     });
 
-    it("indicates its color", function () {
-      expect(playingCard.color()).toBe("red");
-    });
-  });
+    describe("the king of spades", function () {
+      beforeEach(function () {
+        playingCard = new PlayingCard({face: "king", suit: "spades"});
+      });
 
-  describe("the queen of diamonds", function () {
-    beforeEach(function () {
-      playingCard = new PlayingCard({face: "queen", suit: "diamonds"});
-    });
+      it("can be converted into a string", function () {
+        expect(playingCard.toString()).toBe("K♠")
+      });
 
-    it("can be converted into a string", function () {
-      expect(playingCard.toString()).toBe("Q♦")
-    });
-
-    it("indicates its color", function () {
-      expect(playingCard.color()).toBe("red");
-    });
-  });
-
-  describe("the king of spades", function () {
-    beforeEach(function () {
-      playingCard = new PlayingCard({face: "king", suit: "spades"});
+      it("indicates its color", function () {
+        expect(playingCard.color()).toBe("black");
+      });
     });
 
-    it("can be converted into a string", function () {
-      expect(playingCard.toString()).toBe("K♠")
-    });
+    describe("the ace of hearts", function () {
+      beforeEach(function () {
+        playingCard = new PlayingCard({face: "ace", suit: "hearts"});
+      });
 
-    it("indicates its color", function () {
-      expect(playingCard.color()).toBe("black");
-    });
-  });
+      it("can be converted into a string", function () {
+        expect(playingCard.toString()).toBe("A♥")
+      });
 
-  describe("the ace of hearts", function () {
-    beforeEach(function () {
-      playingCard = new PlayingCard({face: "ace", suit: "hearts"});
-    });
-
-    it("can be converted into a string", function () {
-      expect(playingCard.toString()).toBe("A♥")
-    });
-
-    it("indicates its color", function () {
-      expect(playingCard.color()).toBe("red");
+      it("indicates its color", function () {
+        expect(playingCard.color()).toBe("red");
+      });
     });
   });
 });
