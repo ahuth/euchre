5e03e2dc04f8d0b44caab5622775cc2cc41b290e
Andrew <andrew@huth.me>
2016-06-18 22:48:47 -0700

    Add <ShouldOrderUp> to the order up phase

--- app/components/answer.js
+++ app/components/answer.js
@@ -4,7 +4,7 @@ import React from "react";
 
 function Answer({text, handleClick}) {
   return (
-    <div onClick={handleClick} className="answer">
+    <div style={styles.container} onClick={handleClick} className="answer">
       <span>{text}</span>
     </div>
   );
@@ -13,6 +13,17 @@ function Answer({text, handleClick}) {
 Answer.propTypes = {
   text: React.PropTypes.string.isRequired,
   handleClick: React.PropTypes.func.isRequired
+};
+
+var styles = {
+  container: {
+    display: "inline-block",
+    padding: 4,
+    margin: 4,
+    border: "1px solid black",
+    borderRadius: 4,
+    cursor: "pointer"
+  }
 };
 
 export default Answer;
--- app/components/phases/order-up.js
+++ app/components/phases/order-up.js
@@ -3,15 +3,27 @@
 import Kitty from "../kitty";
 import PlayingCard from "../../utils/playing-card";
 import React from "react";
+import ShouldOrderUp from "../questions/should-order-up";
 import Table from "../table";
 
 var OrderUp = React.createClass({
+  handlePass: function () {
+    console.log("Pass!");
+  },
+
+  handleOrderUp: function () {
+    console.log("Order Up!");
+  },
+
   render: function () {
     return (
       <div style={styles.container} className="order-up">
         <Table hands={{north: this.props.hands.north, south: this.props.hands.south, east: this.props.hands.east, west: this.props.hands.west}} />
         <div style={styles.kitty}>
           <Kitty cards={this.props.hands.kitty} />
+        </div>
+        <div style={styles.question}>
+          <ShouldOrderUp handlePass={this.handlePass} handleOrderUp={this.handleOrderUp} />
         </div>
       </div>
     );
@@ -31,6 +43,12 @@ var styles = {
     position: "absolute",
     top: "calc(50% - 51px)",
     right: "calc(50% - 31px)"
+  },
+  question: {
+    position: "absolute",
+    bottom: 120,
+    width: "100%",
+    textAlign: "center"
   }
 }
 
