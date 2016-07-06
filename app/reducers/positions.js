1c996cd3545dcb0865a9fbb3f3ca437f1796df8b
Andrew Huth <ahuth@mavenlink.com>
2016-07-06 15:37:05 -0400

    Rotate the current position when passing an order up

--- app/reducers/positions.js
+++ app/reducers/positions.js
@@ -1,12 +1,27 @@
 "use strict";
 
 import initialState from "./initial-state";
-import {actionTypes} from "../constants";
+import {actionTypes, positions as positionTypes} from "../constants";
 
 function positions(state = initialState.positions, action) {
   switch (action.type) {
+    case actionTypes.PASS_ORDER_UP:
+      return Object.assign({}, state, {current: rotateCurrent(state.current)});
     default:
       return state;
+  }
+}
+
+function rotateCurrent(current) {
+  switch (current) {
+    case positionTypes.NORTH:
+      return positionTypes.EAST;
+    case positionTypes.SOUTH:
+      return positionTypes.WEST;
+    case positionTypes.EAST:
+      return positionTypes.SOUTH;
+    case positionTypes.WEST:
+      return positionTypes.NORTH;
   }
 }
 
--- spec/reducers/positions-spec.js
+++ spec/reducers/positions-spec.js
@@ -1,12 +1,33 @@
 "use strict";
 
 import positions from "reducers/positions";
+import {passOrderUp} from "../../app/actions";
 
 describe("reducers", function () {
   describe("positions", function () {
     it("returns the current state for unknown actions", function () {
       var currentState = "hi";
       expect(positions(currentState, {type: "WAT"})).toEqual(currentState);
+    });
+
+    describe("for passOrderUp", function () {
+      var action, state;
+
+      beforeEach(function () {
+        action = passOrderUp();
+        state = {current: "west", dealer: "south", initial: "west"};
+        state = positions(state, action);
+      });
+
+      it("rotates the current position clockwise", function () {
+        expect(state.current).toBe("north");
+        state = positions(state, action);
+        expect(state.current).toBe("east");
+        state = positions(state, action);
+        expect(state.current).toBe("south");
+        state = positions(state, action);
+        expect(state.current).toBe("west");
+      });
     });
   });
 });
