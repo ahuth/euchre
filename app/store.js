73414f767266fc221d4328d61402e9a4ae8b1e6c
Andrew Huth <ahuth@mavenlink.com>
2016-07-07 16:01:23 -0400

    Add missing semi-colon

--- app/store.js
+++ app/store.js
@@ -2,7 +2,7 @@
 
 import * as reducers from "./reducers";
 import thunkMiddleware from "redux-thunk";
-import {applyMiddleware, combineReducers, createStore} from "redux"
+import {applyMiddleware, combineReducers, createStore} from "redux";
 
 var root = combineReducers(reducers);
 var store = createStore(root, applyMiddleware(thunkMiddleware));
