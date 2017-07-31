6f1933c32e2fbf668d9fd104eca4f596b91bc68c
Andrew <andrew@huth.me>
2017-07-30 17:09:13 -0700

    Use layout primitives to display the cards

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,14 @@
 import React from "react"
 import Card from "./card"
+import {Box, Flex} from "reflexbox"
 
 export default function App() {
   return (
-    <div>
-      <Card rank="A" suit="hearts" />
-      <Card rank="K" suit="clubs" />
-      <Card rank="Q" suit="diamonds" />
-      <Card rank="J" suit="spades" />
-    </div>
+    <Flex p={1}>
+      <Box p={1}><Card rank="A" suit="hearts" /></Box>
+      <Box p={1}><Card rank="K" suit="clubs" /></Box>
+      <Box p={1}><Card rank="Q" suit="diamonds" /></Box>
+      <Box p={1}><Card rank="J" suit="spades" /></Box>
+    </Flex>
   )
 }
