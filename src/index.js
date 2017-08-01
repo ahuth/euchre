fbfe630ed3c38051c5d252f81ba3e39d6bd70e10
Andrew <andrew@huth.me>
2017-07-31 22:03:30 -0700

    Standardize quote and semi-colon style

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react"
+import ReactDOM from "react-dom"
 import {Provider} from "react-redux"
-import App from './containers/app';
+import App from "./containers/app"
 import store from "./store"
-import './index.css';
+import "./index.css"
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>
-  , document.getElementById('root')
-);
+  , document.getElementById("root")
+)
