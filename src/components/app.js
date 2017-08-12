68e05c6f5cfe5c83b57dd46236483882e290813a
Andrew <andrew@huth.me>
2017-08-12 16:09:42 -0700

    Show the "hand" part way down the page

--- src/components/app.js
+++ src/components/app.js
@@ -4,14 +4,19 @@ import {Box, Flex} from "reflexbox"
 
 export default function App({cards}) {
   return (
-    <Flex p={1}>
-      {cards.map((card) => {
-        return (
-          <Box key={`${card.rank}${card.suit}`} p={1}>
-            <Card rank={card.rank} suit={card.suit} />
-          </Box>
-        )
-      })}
+    <Flex column justify="space-around" style={{height: "100vh"}}>
+      <Box w={1}></Box>
+      <Box w={1}>
+        <Flex p={1} justify="center">
+          {cards.map((card) => {
+            return (
+              <Box key={`${card.rank}${card.suit}`} p={1}>
+                <Card rank={card.rank} suit={card.suit} />
+              </Box>
+            )
+          })}
+        </Flex>
+      </Box>
     </Flex>
   )
 }
