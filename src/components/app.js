2a56e57899757d726f95b901329127ef19e1992c
Andrew <andrew@huth.me>
2017-08-12 17:20:35 -0700

    Extract a <Pile> component

--- src/components/app.js
+++ src/components/app.js
@@ -1,24 +1,12 @@
 import React from "react"
 import {Box, Flex} from "reflexbox"
-import Card from "./card"
 import Hand from "./hand"
-import Rotate from "./rotate"
-import {Card as CardType} from "../types"
+import Pile from "./pile"
 
 export default function App({cards, handleCardClick, played}) {
   return (
     <Flex column justify="space-around" style={{height: "100vh"}}>
-      <Flex w={1} justify="center" style={{position: "relative"}}>
-        {played.map(({card, rotation}) => {
-          return (
-            <Box key={CardType.uniqueKey(card)} style={{position: "absolute"}}>
-              <Rotate by={rotation}>
-                <Card rank={card.rank} suit={card.suit} />
-              </Rotate>
-            </Box>
-          )
-        })}
-      </Flex>
+      <Pile cards={played} />
       <Box w={1}>
         <Hand cards={cards} onCardClick={handleCardClick} />
       </Box>
