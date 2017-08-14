0d18a1787c6c3b912af21e0420919dbc138547c3
Andrew <andrew@huth.me>
2017-08-13 20:24:49 -0700

    Reset the "played" state when dealing cards

--- src/reducers/played.js
+++ src/reducers/played.js
@@ -2,6 +2,8 @@ import initialState from "./initial-state"
 
 export default function Played(state = initialState.played, action) {
   switch (action.type) {
+    case "DEAL_HANDS":
+      return initialState.played
     case "PLAY_CARD":
       return state.concat([{
         card: action.payload.card,
