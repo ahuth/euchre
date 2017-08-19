078610bf2d0afcf772a677f4f5317b9efd182148
Andrew Huth <ahuth@mavenlink.com>
2017-08-19 10:41:47 -0700

    Rename <Layout> to <Table>

--- src/components/app.js
+++ src/components/app.js
@@ -2,14 +2,14 @@ import React from "react"
 import noop from "lodash/noop"
 import {Flex} from "reflexbox"
 import Card from "./card"
-import Layout from "./layout"
+import Table from "./table"
 
 export default function App({east, north, south, upCard, west}) {
   return (
-    <Layout south={south} onCardClick={noop}>
+    <Table south={south} onCardClick={noop}>
       <Flex justify="center">
         <Card rank={upCard.rank} suit={upCard.suit} />
       </Flex>
-    </Layout>
+    </Table>
   )
 }
