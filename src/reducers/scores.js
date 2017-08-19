815f73be3b5c2ec7607a5368a455779832c86274
Andrew Huth <ahuth@mavenlink.com>
2017-08-18 20:46:57 -0700

    Re-initialize the scores when dealing hands

--- src/reducers/scores.js
+++ src/reducers/scores.js
@@ -2,6 +2,8 @@ import initialState from "./initial-state"
 
 export default function Scores(state = initialState.scores, action) {
   switch (action.type) {
+    case "DEAL_HANDS":
+      return {north: 0, west: 0}
     default:
       return state
   }
