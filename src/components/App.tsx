99597d6f2900d8d3692074df156af679e90798cc
Andrew Huth <andrew@huth.me>
2019-05-16 07:14:19 -0700

    Use union instead of enum for the suits

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Card from './Card';
-import { Suit } from '../utils/card';
 
 export default function App() {
-  return <Card rank="A" suit={Suit.hearts} />
+  return <Card rank="A" suit="hearts" />
 }
--- src/utils/card.ts
+++ src/utils/card.ts
@@ -1,8 +1,2 @@
-export type Rank = '4' | '6' | '9' | '10' | 'J' | 'Q' | 'K' | 'A'
-
-export enum Suit {
-  clubs = 'clubs',
-  diamonds = 'diamonds',
-  hearts = 'hearts',
-  spades = 'spades',
-}
+export type Rank = '4' | '6' | '9' | '10' | 'J' | 'Q' | 'K' | 'A';
+export type Suit = 'clubs' | 'diamonds' | 'hearts' | 'spades';
