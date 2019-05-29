461ea111bab218217f3c64b73e49467e2c40d657
Andrew Huth <andrew@huth.me>
2019-05-28 21:54:39 -0700

    Add all the actions I can think of
    There may be more, but these are at least the basics.

--- src/reducer.ts
+++ src/reducer.ts
@@ -1,7 +1,25 @@
 import * as Game from './utils/game';
+import { Type as Card, Suit } from './utils/card';
 
-export type Action = StartAction;
-type StartAction = { type: 'start' };
+export type Action =
+  | CallTrump
+  | CallTrumpAlone
+  | Discard
+  | Pass
+  | PickItUp
+  | PickItUpAlone
+  | PlayCard
+  | StartAction
+;
+
+type CallTrump = { type: 'call_trump', suit: Suit }
+type CallTrumpAlone = { type: 'call_trump_alone', suit: Suit }
+type Discard = { type: 'discard', card: Card }
+type Pass = { type: 'pass' }
+type PickItUp = { type: 'pick_it_up' }
+type PickItUpAlone = { type: 'pick_it_up_alone' }
+type PlayCard = { type: 'play_card', card: Card }
+type StartAction = { type: 'start' }
 
 export default function reducer(state: Game.Type, action: Action) {
   switch (action.type) {
