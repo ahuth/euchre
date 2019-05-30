1f90a3bde4dd68c7d02439ceb558ca360e181c41
Andrew Huth <andrew.huth@airbnb.com>
2019-05-29 21:00:48 -0700

    Use getter to get the top card

--- src/components/PickingUp.tsx
+++ src/components/PickingUp.tsx
@@ -4,6 +4,7 @@ import Card from './Card';
 import Rotate from './Rotate';
 import { Action } from '../reducer';
 import { Type as Game } from '../utils/game';
+import { peek } from '../utils/deck';
 
 type Props = {
   dispatch: React.Dispatch<Action>,
@@ -12,7 +13,7 @@ type Props = {
 
 export default function PickingUp({ dispatch, state }: Props) {
   const rotation = useMemo(() => random(-2, 2), []);
-  const topCard = state.round.deck[0];
+  const topCard = peek(state.round.deck);
 
   return (
     <Rotate by={rotation}>
