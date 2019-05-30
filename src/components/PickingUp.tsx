02f6eadde61c8181ce19d24aab41dba5a792f7d8
Andrew Huth <andrew.huth@airbnb.com>
2019-05-29 21:03:06 -0700

    Use namespaces where possible

--- src/components/PickingUp.tsx
+++ src/components/PickingUp.tsx
@@ -3,17 +3,17 @@ import { random } from 'lodash';
 import Card from './Card';
 import Rotate from './Rotate';
 import { Action } from '../reducer';
-import { Type as Game } from '../utils/game';
-import { peek } from '../utils/deck';
+import * as Game from '../utils/game';
+import * as Deck from '../utils/deck';
 
 type Props = {
   dispatch: React.Dispatch<Action>,
-  state: Game,
+  state: Game.Type,
 }
 
 export default function PickingUp({ dispatch, state }: Props) {
   const rotation = useMemo(() => random(-2, 2), []);
-  const topCard = peek(state.round.deck);
+  const topCard = Deck.peek(state.round.deck);
 
   return (
     <Rotate by={rotation}>
