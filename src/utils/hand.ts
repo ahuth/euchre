85a543b7b8a04ab2f6ef45850d603439b0c3a1c2
Andrew Huth <andrew@huth.me>
2019-05-31 21:04:37 -0700

    Use a simple array to represent a hand

--- src/utils/hand.ts
+++ src/utils/hand.ts
@@ -1,8 +1,6 @@
 import * as Card from './card';
 
-interface Hand {
-  cards: Array<Card.Type>,
-}
+type Hand = Array<Card.Type>;
 
 export type Type = Hand;
 
@@ -10,27 +8,19 @@ export type Type = Hand;
  * Create a new hand.
  */
 export function create(): Hand {
-  return {
-    cards: [],
-  };
+  return [];
 }
 
 /**
  * Add any number of cards to this hand.
  */
 export function deal(hand: Hand, cards: Array<Card.Type>): Hand {
-  return {
-    ...hand,
-    cards: hand.cards.concat(cards),
-  };
+  return hand.concat(cards);
 }
 
 /**
  * Remove a card from the hand.
  */
 export function remove(hand: Hand, card: Card.Type): Hand {
-  return {
-    ...hand,
-    cards: hand.cards.filter(c => !Card.equal(c, card)),
-  };
+  return hand.filter(c => !Card.equal(c, card));
 }
