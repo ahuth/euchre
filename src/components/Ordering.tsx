efb31cc00f452b544cf5f9cfba62faea0d1c1234
Andrew Huth <git@huth.me>
2025-01-04 11:33:38 -0500

    Directly send event to store from sub-component
    Instead of passing down a callback

--- src/components/Ordering.tsx
+++ src/components/Ordering.tsx
@@ -1,4 +1,3 @@
-import {useCallback} from 'react';
 import {store, useSelector} from '../store';
 import Display from './Display';
 
@@ -8,42 +7,28 @@ import Display from './Display';
 export default function Ordering() {
   const current = useSelector(store, (state) => state.context.current);
 
-  const orderUp = useCallback(() => {
-    store.send({type: 'orderUp'});
-  }, []);
-
-  const pass = useCallback(() => {
-    store.send({type: 'passOrderUp'});
-  }, []);
-
   return (
     <Display
-      slot1={current === 0 && <Selector onOrderUp={orderUp} onPass={pass} />}
-      slot2={current === 1 && <Selector onOrderUp={orderUp} onPass={pass} />}
-      slot3={current === 2 && <Selector onOrderUp={orderUp} onPass={pass} />}
-      slot4={current === 3 && <Selector onOrderUp={orderUp} onPass={pass} />}
+      slot1={current === 0 && <Selector />}
+      slot2={current === 1 && <Selector />}
+      slot3={current === 2 && <Selector />}
+      slot4={current === 3 && <Selector />}
     />
   );
 }
 
-function Selector({
-  onOrderUp,
-  onPass,
-}: {
-  onOrderUp: () => void;
-  onPass: () => void;
-}) {
+function Selector() {
   return (
     <div className="flex items-end gap-2">
       <button
         className="h-12 rounded bg-green-500 px-4 py-2 text-white"
-        onClick={onOrderUp}
+        onClick={() => store.send({type: 'orderUp'})}
       >
         Order Up
       </button>
       <button
         className="h-12 rounded bg-red-500 px-4 py-2 text-white"
-        onClick={onPass}
+        onClick={() => store.send({type: 'passOrderUp'})}
       >
         Pass
       </button>
