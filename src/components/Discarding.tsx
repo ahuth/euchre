3f76f4cd804e66b4a69ed932e0e25cecd1220462
Andrew Huth <git@huth.me>
2025-01-03 22:16:51 -0500

    Extract one function for discarding

--- src/components/Discarding.tsx
+++ src/components/Discarding.tsx
@@ -1,4 +1,6 @@
+import {useCallback} from 'react';
 import {store, useSelector} from '../store';
+import type {Card} from '../cards';
 import Hand from './Hand';
 
 export default function Discarding() {
@@ -9,6 +11,10 @@ export default function Discarding() {
   const kitty = useSelector(store, (state) => state.context.kitty);
   const dealer = useSelector(store, (state) => state.context.dealer);
 
+  const discard = useCallback((card: Card) => {
+    store.send({type: 'discard', card});
+  }, []);
+
   return (
     <div className="flex flex-col gap-4">
       <div className="flex flex-wrap gap-2">
@@ -16,9 +22,7 @@ export default function Discarding() {
           title="Hand 1"
           cards={hand1}
           dealer={dealer === 0}
-          onCardClick={
-            dealer === 0 && ((card) => store.send({type: 'discard', card}))
-          }
+          onCardClick={dealer === 0 && discard}
         />
       </div>
       <div className="flex flex-wrap gap-2">
@@ -26,9 +30,7 @@ export default function Discarding() {
           title="Hand 2"
           cards={hand2}
           dealer={dealer === 1}
-          onCardClick={
-            dealer === 1 && ((card) => store.send({type: 'discard', card}))
-          }
+          onCardClick={dealer === 1 && discard}
         />
       </div>
       <div className="flex flex-wrap gap-2">
@@ -36,9 +38,7 @@ export default function Discarding() {
           title="Hand 3"
           cards={hand3}
           dealer={dealer === 2}
-          onCardClick={
-            dealer === 2 && ((card) => store.send({type: 'discard', card}))
-          }
+          onCardClick={dealer === 2 && discard}
         />
       </div>
       <div className="flex flex-wrap gap-2">
@@ -46,9 +46,7 @@ export default function Discarding() {
           title="Hand 4"
           cards={hand4}
           dealer={dealer === 3}
-          onCardClick={
-            dealer === 3 && ((card) => store.send({type: 'discard', card}))
-          }
+          onCardClick={dealer === 3 && discard}
         />
       </div>
       <Hand title="Kitty" cards={kitty} dealer={false} />
